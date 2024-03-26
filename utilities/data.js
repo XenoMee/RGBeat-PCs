0d14c10945f395989a0adb6b88d8175bda46e44a
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-26 21:56:54 +0200

    add uuids to each shop item

--- utilities/data.js
+++ utilities/data.js
@@ -2,7 +2,7 @@ const itemsArray = [
   {
     name: 'Pink AMD Fan',
     tags: ['Fan', 'AMD', 'Pink color'],
-    id: 0,
+    id: '806825e2-5736-416c-ac20-610d23b92d72',
     price: 14,
     image: './public/images/memorySlots/pink-fan-with-RGB-memory-slots.png',
   },
@@ -11,14 +11,14 @@ const itemsArray = [
     tags: ['Memory Slot', 'HyperX', 'DDR4', 'Blue Color'],
     price: 12,
     image: './public/images/memorySlots/blue-hyperX-memory-slots.png',
-    id: 1,
+    id: '400bdc0e-b27d-4980-8815-a2c53d2cbd00',
   },
   {
     name: 'Corsair RGB Memory Slots',
     tags: ['Memory Slot', 'Corsair', 'DDR4', 'Multicolor'],
     price: 12,
     image: './public/images/memorySlots/RGB-memory-slots.png',
-    id: 2,
+    id: 'c332db98-2a7b-4ac2-a812-c5119cb18bf8',
   },
 ];
 
