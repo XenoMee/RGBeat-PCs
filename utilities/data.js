770ff7a59d6dbb1cda2d95d4b50ea9ad37717310
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-31 20:11:24 +0300

    update images path

--- utilities/data.js
+++ utilities/data.js
@@ -4,20 +4,20 @@ const itemsArray = [
     tags: ['Fan', 'AMD', 'Pink color'],
     id: '806825e2-5736-416c-ac20-610d23b92d72',
     price: 50,
-    image: './public/images/memorySlots/pink-fan-with-RGB-memory-slots.png',
+    image: './images/pink-fan-with-RGB-memory-slots.png',
   },
   {
     name: 'Blue HyperX Memory Slots',
     tags: ['Memory Slot', 'HyperX', 'DDR4', 'Blue Color'],
     price: 235,
-    image: './public/images/memorySlots/blue-hyperX-memory-slots.png',
+    image: './images/blue-hyperX-memory-slots.png',
     id: '400bdc0e-b27d-4980-8815-a2c53d2cbd00',
   },
   {
     name: 'Corsair RGB Memory Slots',
     tags: ['Memory Slot', 'Corsair', 'DDR4', 'Multicolor'],
     price: 460,
-    image: './public/images/memorySlots/RGB-memory-slots.png',
+    image: './images/RGB-memory-slots.png',
     id: 'c332db98-2a7b-4ac2-a812-c5119cb18bf8',
   },
 ];
